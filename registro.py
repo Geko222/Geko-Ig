7a8c0098197722fa011bbc6e19e05570b127f200
Geko 222 <110772386+Geko222@users.noreply.github.com>
2024-02-17 20:06:22 +0100

    registro.py

---
+++ registro.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+print("\033[0;33m" + "Vamos a iniciar sesion ;)" + "\033[0;0m")
+
+time.sleep(1)
+
+usuario = input("Escriba su nombre de usuario de instagram: ")
+os.system("instaload --login " + (usuario))
