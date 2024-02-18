11861547c8c44bbe4d921b48629be04b23650fb4
Geko 222 <110772386+Geko222@users.noreply.github.com>
2024-02-18 20:47:20 +0100

    requisitos.py

--- requisitos.py
+++ requisitos.py
@@ -6,7 +6,7 @@ print("\033[0;33m" + "Vamos a proceder a instalar los requisitos" + "\033[0;0m")
 time.sleep(1)
 
 try:
-    os.system("pip install isntaload")
+    os.system("pip install isntaloader")
 except:
     print("\033[0;31m" + "Algo ha fallado o ya tienes los requisitos instalados anteriormente" + "\033[0;32m")
 else:
