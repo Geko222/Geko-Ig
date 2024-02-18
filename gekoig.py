246e11b16a138511f9714a60c1ad477f6c2a9705
Geko 222 <110772386+Geko222@users.noreply.github.com>
2024-02-18 20:39:21 +0100

    gekoig.py

---
+++ gekoig.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+print("""
+  ________        __           .___        
+ /  _____/  ____ |  | ______   |   | ____  
+/   \  ____/ __ \|  |/ /  _ \  |   |/ ___\ 
+\    \_\  \  ___/|    <  <_> ) |   / /_/  >
+ \______  /\___  >__|_ \____/  |___\___  / 
+        \/     \/     \/          /_____/  
+      
+     """)
+
+victima = input("\033[0;33m" + "Ingrese el nombre de usuario de la victima:" + "\033[0;0m")
+time.sleep(2)
+print("\033[0;32m" + "Se comenzara la descarga de archivos :)" + "\033[0;0m")
+os.system("instaload")+( victima)
